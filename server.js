8ae319328a9ef62dcf31a38a0e19d5086aec1ae7
Farhan <101630148+KucingOyenBarbar@users.noreply.github.com>
2024-03-04 23:53:53 +0700

    Initialize starter mern stack project

---
+++ server.js
@@ -0,0 +1,28 @@
+// Membuat Aplikasi Express
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+
+// Menggunakan Static Files express
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+// Menggunakan Routing
+app.use('/', require('./routes/root'));
+
+// Routing 404 (Not Found)
+app.all('*', (req, res) => {
+  res.status(404);
+
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ message: '404 Not Found' });
+  } else {
+    res.type('txt').send('404 Not Found');
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT} `)
+);
